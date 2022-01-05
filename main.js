d45dce751d1b9d40a243e85f92303f7c82473e77
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2022-01-05 11:39:54 +0600

    add nav menu

---
+++ main.js
@@ -0,0 +1,23 @@
+const navCloseEl = document.querySelector('.nav_close');
+const navList = document.querySelector('.nav_list');
+const navIconEl = document.querySelector('.nav_icon');
+const navBgOverlayEl = document.querySelector('.nav_bgOverlay');
+
+window.addEventListener('DOMContentLoaded', () =>{
+  document.body.style.visibility = 'visible';
+});
+const navOpen = () => {
+    navList.classList.add('show');
+    navBgOverlayEl.classList.add('active');
+    document.body.style= 'visibility: visible; height: 100vh; width:100vw; overflow:hidden;';
+}
+const navClose = () => {
+    navList.classList.remove('show');
+    navBgOverlayEl.classList.remove('active');
+    document.body.style= 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose);
